a3ce37e440c7d9edf012cc88c3e54a8c7cae56b3
Augusto Henrique <augusto.henrique.jofre.silva@gmail.com>
2026-02-02 16:06:08 +0100

    Implementing streamlit widgets in app.py

--- app.py
+++ app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.header('Flipping a coin')
 
+number_of_trials = st.slider('Number of trials?', 1, 1000, 10)
+start_button = st.button('Execute')
+
+if start_button:
+	st.write(f'Running the experiment with {number_of_trials} attempts.')
+
 st.write('Not yet a functional app. Under development.')
